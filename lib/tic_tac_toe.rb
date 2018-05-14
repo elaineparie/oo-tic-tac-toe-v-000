a36398a736a88d315c8db245aa4f851dfbd866ca
Elaine Anderson <eparie@bu.edu>
2018-05-14 20:24:16 +0000

    automatically backed up by learn

---
+++ lib/tic_tac_toe.rb
@@ -0,0 +1,15 @@
+class TicTacToe
+
+  def initialize(board = [" ", " ", " ", " ", " ", " ", " ", " ", " "])
+    @board = board
+  end 
+
+  def board= (board)
+    @board = board 
+  end 
+
+  def board 
+    @board 
+  end
+  
+end
